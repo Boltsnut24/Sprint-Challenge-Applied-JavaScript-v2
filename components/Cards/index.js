3a01ee94d8979a4c21678ef8f53f92502e20c641
Brad <Brad@BRADSPC>
2019-07-12 09:44:42 -0600

    Can add a card component successfully, need to iterate through data and add all cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,38 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function MakeCard(data){
+  const card = document.createElement('div')
+  const headlineBox = document.createElement('div')
+  const authorBox = document.createElement('div')
+  const imgBox = document.createElement('div')
+  const img = document.createElement('img')
+  const author = document.createElement('span')
+
+  card.appendChild(headlineBox)
+  card.appendChild(authorBox)
+  authorBox.appendChild(imgBox)
+  imgBox.appendChild(img)
+  authorBox.appendChild(author)
+
+  card.classList.add('card')
+  headlineBox.classList.add('headline')
+  authorBox.classList.add('author')
+  imgBox.classList.add('img-container')
+
+  headlineBox.textContent = data.headline
+  img.src = data.authorPhoto
+  author.textContent = `By ${data.authorName}`
+  console.log(card)
+  return card
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then( response => {
+    console.log(response)
+    document.querySelector('.cards-container').appendChild(MakeCard(response.data.articles.bootstrap[0]))
+  })
+  .catch(error => {
+    console.log("Error: ", error)
+  })
